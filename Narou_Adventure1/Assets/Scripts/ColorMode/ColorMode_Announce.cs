48726570f3555d2a4bacec2c7e20d0f8477f4fa7
Kazuma Tomida <ktomppp@gmail.com>
2020-01-22 21:44:01 +0900

    Add color mode to announce
    アナウンスにカラーモードの追加

---
+++ Narou_Adventure1/Assets/Scripts/ColorMode/ColorMode_Announce.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ColorMode
+{
+    /// <summary>
+    /// main.SR.colorModeが0の時は変更なし
+    /// 1のときは配列colorの1番目.color[0]が入る
+    /// 2のときは配列colorの２番目.color[1]が入る
+    /// ...
+    /// </summary>
+    public class ColorMode_Announce : BASE
+    {
+        Announce announce;
+        public Color[] color;
+
+        private void Awake()
+        {
+            StartBASE();
+            announce = GetComponent<Announce>();
+            Apply();
+        }
+
+        void Apply()
+        {
+            if (main.SR.colorMode == 0)
+            {
+                return;//デフォルトのまま
+            }
+            if (main.SR.colorMode > 0 && main.SR.colorMode <= color.Length)
+            {
+                announce.baseColor = color[main.SR.colorMode - 1];
+            }
+        }
+    }
+}
